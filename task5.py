c31c17741fe43c0b1ef388fe0f8fefeb7255498e
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-03-04 07:41:52 -0600

    Update task5.py

--- task5.py
+++ task5.py
@@ -3,6 +3,6 @@ escaped = True
 if escaped:
     outcome = "Legend: you have escaped."
 else:
-    outcome = "Doom: you got found."
+    outcome = "Doom: you got found by spiderman."
 print(outcome)
 
