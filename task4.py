949f3714545f7a6f80dccb0df63b525cb198ab0d
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-03-04 07:42:26 -0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -2,6 +2,6 @@ drawbridge_raised = True
 if not drawbridge_raised:
     outcome = "Thunder: you can cross."
 else:
-    outcome = "Doom: you cannot cross."
+    outcome = "Doom: you cannot cross the golden gate bridge."
 print(outcome)
 
