402c1e630fe335899ad3e797a838fb89bcb85bf2
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-03-04 07:42:47 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -2,6 +2,6 @@ guard_awake = False
 if not guard_awake:
     outcome = "Shadow: sneak past."
 else:
-    outcome = "Doom: you got caught."
+    outcome = "Doom: you got caught by the police."
 print(outcome)
 
